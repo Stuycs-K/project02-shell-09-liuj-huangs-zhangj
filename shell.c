2efe033646517329d7404f77f7fb434a9262b72d
Jason Liu <sumrandomhooman@gmail.com>
2024-12-02 18:32:06 -0500

    added basic functionality

--- shell.c
+++ shell.c
@@ -6,20 +6,36 @@
 #include <errno.h>
 #include <string.h>
 
-void parse_args(char* line, char** arg_ary){
+void parse_args( char * line, char ** arg_ary ){
   int counter = 0;
   while((arg_ary[counter] = strsep(&line, " "))){
     counter++;
   }
 }
+
 int main(){
   char buffer[256];
-  fgets(buffer, 255, stdin);
-  char** args;
-  char* line;
-  strcpy(line, buffer);
-  parse_args(line, args);
-  printf("%s\n", args);
-  execvp(args[0], args);
+  while(fgets(buffer, 255, stdin)){ // awaiting more inputs
+    char* copy = (char *) malloc(256);
+    strcpy(copy, buffer);
+
+    char* function;
+    while((function = strsep(&copy, ";"))){ // splitting into multipl functions
+      char* args[100];
+      parse_args(function, args);
+      pid_t child; // making child to sacrifice to function
+      child = fork();
+      if(child<0){ // error handling
+        perror("fork fail");
+        exit(1);
+      }
+      if(child == 0){
+        execvp(args[0], args); // child running function
+      }
+      else{
+        wait(NULL); // awaiting child death
+      }
+    }
+  }
   return 0;
 }
