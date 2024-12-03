e6ae857b417cc6e5185fd2a176fb42b674d16529
Julien Zhang <jzhang52@stuy.edu>
2024-12-03 14:30:42 -0500

    fixed execvp

--- shell.c
+++ shell.c
@@ -16,28 +16,31 @@ void parse_args( char * line, char ** arg_ary ){
 int main(){
   char buffer[256];
   char cwd[256];
-  while(1){ // awaiting more inputs
-    getcwd(cwd, sizeof(cwd));
-    printf("%s $", cwd);
-    fgets(buffer, 255, stdin);
-    char* copy = (char *) malloc(256);
-    strcpy(copy, buffer);
-    char* function;
-    while((function = strsep(&copy, ";"))){ // splitting into multiple functions
-      char* args[100];
-      parse_args(function, args);
-      pid_t child; // making child to sacrifice to function
-      child = fork();
-      if(child<0){ // error handling
+  getcwd(cwd, sizeof(cwd));
+  printf("%s $", cwd);
+  fgets(buffer, 255, stdin);
+  char* copy = (char *) malloc(256);
+  sscanf(buffer, "%[^\n]", copy);
+  char* function;
+  while((function = strsep(&copy, ";"))){ // splitting into multiple functions
+    char* args[100];
+    parse_args(function, args);
+    pid_t child; // making child to sacrifice to function
+    child = fork();
+    if(child<0){ // error handling
+      perror("fork fail");
+      exit(1);
+    }
+    if(child == 0){
+      int exec;
+      exec = execvp(args[0], args); // child running function
+      if(exec<0){ // error handling
         perror("fork fail");
         exit(1);
       }
-      if(child == 0){
-        execvp(args[0], args); // child running function
-      }
-      else{
-        wait(NULL); // awaiting child death
-      }
+    }
+    else{
+      wait(NULL); // awaiting child death
     }
   }
   return 0;
