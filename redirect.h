ee75f49c99e5579e97bb2fd32e9a8ab401bdc1ee
Julien Zhang <jzhang52@stuy.edu>
2024-12-06 14:48:12 -0500

    separated redirect

--- redirect.h
+++ redirect.h
@@ -1,6 +1,6 @@
 #ifndef REDIRECT_H
 #define REDIRECT_H
-int redir(char* args[], int argsLen);
 void stdoutRedirExec(char* path, char* args[]);
 void stdinRedirExec(char* path, char* args[]);
+int redir(char* args[], int argsLen);
 #endif
