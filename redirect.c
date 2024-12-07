95236878842b5aa764b22e1a49cea470a5a1ebc7
Jason Liu <sumrandomhooman@gmail.com>
2024-12-07 00:11:58 -0500

    made an eldritch monstrocity for pipe

--- redirect.c
+++ redirect.c
@@ -34,29 +34,125 @@ void stdinRedirExec(char* path, char* args[]){
   }
 }
 
+int PipeRedirExec(char* args[], int pipeLocation, int argsLen){
+  pid_t bb;
+  bb = fork();
+  if(bb < 0){
+      perror("fork fail");
+      exit(1);
+  }
+  if(bb == 0){
+    remove("temp.txt");
+    int fd1 = open("temp.txt", O_WRONLY | O_APPEND | O_CREAT, 0600);
+    dup(1);
+    dup2(fd1, 1);
+    int exec;
+    close(fd1);
+
+    char* args1input[100];
+
+    char * arg1 = malloc(sizeof(args));
+    strcpy(arg1, args[0]);
+    for(int i = 1; i < pipeLocation; i++){
+      strcat(arg1, " ");
+      strcat(arg1, args[i]);
+    }
+
+    int len = parse_args(arg1, args1input);
+    if(len > 1 && strcmp(args1input[1],"<") == 0){
+      char* newarg1input[100];
+      for(int i = 0; i < len-2; i++){
+        newarg1input[i] = args1input[i];
+      }
+      stdinRedirExec(args1input[len - 1], newarg1input);
+    }else{
+      exec = execvp(args1input[0], args1input);
+      if (exec<0){
+        perror("stoutRedirect fail: error A");
+        exit(1);
+      }
+    }
+    return 0;
+  }
+  else{
+      wait(NULL);
+
+      int fd1 = open("temp.txt", O_RDONLY);
+      dup(0);
+      dup2(fd1, 0);
+      int exec;
+      close(fd1);
+
+      char* args2input[100];
+
+      char * arg2 = malloc(sizeof(args));
+      strcpy(arg2, args[pipeLocation+1]);
+      int foundredir = 1;
+      for(int i = pipeLocation + 2; i < argsLen; i++){
+          if(strcmp(args[i], ">") == 0){
+            foundredir = 0;
+            break;
+          }
+          strcat(arg2, " ");
+          strcat(arg2, args[i]);
+      }
+
+      int len = parse_args(arg2, args2input);
+      remove("temp.txt");
+
+      if(foundredir == 0){
+        remove(args[len+2+pipeLocation]);
+        int f = open(args[len+2+pipeLocation], O_WRONLY | O_APPEND | O_CREAT, 0600);
+        dup(1);
+        dup2(f, 1);
+        close(f);
+      }
+      exec = execvp(args2input[0], args2input);
+      if (exec<0){
+        perror("stdinRedirect fail: error B");
+        exit(1);
+      }
+      
+      return 0;
+  }
+}
+
 int redir(char* args[], int argsLen){
   char* path;
+  int MetalPipe = 0;
+  int pipeLocation = 0;
   int stdoutRedir = 0;
   int stdinRedir = 0;
   for (int i = 0; i<argsLen; i++){
     if (strcmp(args[i], "|") == 0){
-       MetalPipe = 1;
-       args[i] = NULL;
-       pipelocation = i;
-       break;
-   }
-    if (strcmp(args[i], ">") == 0){
-      path = args[i+1];
-      stdoutRedir = 1;
-      args[i] = NULL;
-      break;
+        MetalPipe = 1;
+        args[i] = NULL;
+        pipeLocation = i;
+        break;
     }
-    if (strcmp(args[i], "<") == 0){
-      path = args[i+1];
-      stdinRedir = 1;
-      args[i] = NULL;
-      break;
+  }
+  if(MetalPipe != 1){
+    for (int i = 0; i<argsLen; i++){
+      if(MetalPipe == 1){
+        break;
+      }
+      if (strcmp(args[i], ">") == 0){
+        path = args[i+1];
+        stdoutRedir = 1;
+        args[i] = NULL;
+        break;
+      }
+      if (strcmp(args[i], "<") == 0){
+        path = args[i+1];
+        stdinRedir = 1;
+        args[i] = NULL;
+        break;
+      }
     }
+  }
+  if (MetalPipe == 1){
+    PipeRedirExec(args, pipeLocation, argsLen);
+    return 1;
   }
   if (stdoutRedir == 1){
     stdoutRedirExec(path, args);
@@ -67,34 +163,4 @@ int redir(char* args[], int argsLen){
     return 1;
   }
   return 0;
-}
-
-int pipe(char* args[]){
-  pid_t bb;
-  bb = fork();
-  if(bb < 0){
-      perror("fork fail");
-      exit(1);
-  }
-  if(bb == 0){
-      char * arg1 = malloc(sizeof(args));
-      strcpy(arg1, args[0]);
-      for(int i = 1; i < pipelocation; i++){
-          strcat(arg1, args[i]);
-      }
-      strcat(arg1, " > temp.txt");
-      execute(arg1);
-      return 0;
-  }
-  else{
-      wait(NULL);
-      char * arg2 = malloc(sizeof(args));
-      strcpy(args2, args[pipelocation+1]);
-      for(int i = pipelocation + 2; i < argslen; i++){
-          strcat(arg2, arg[i]);
-      }
-      strcat(arg2, " < temp.txt");
-      execute(arg2);
-      remove("temp.txt");
-  }
 }
