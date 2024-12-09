d60e0e94786bfeed98fb0cb4c2c73f342d81aa9c
Stanley Huang <shuang50@stuy.edu>
2024-12-08 22:53:18 -0500

    fixed issue with printing path

--- path.c
+++ path.c
@@ -9,13 +9,11 @@
 void printPath(){ // Takes in no args, returns void, grabs the path of the working directory and home directory, printing out the path and then shortening if necessary, called by main before it takes input from stdin
   char * currCwd = malloc(256);
   char * homeCwd = malloc(256);
-  char * beginning = malloc(256); //used to check the beginning of the path of the current working directory
+  char * beginning = calloc(256, sizeof(char)); //used to check the beginning of the path of the current working directory
   getcwd(currCwd, 256); //Get path of current working directory
   strcpy(homeCwd, getenv("HOME")); //Get path of home directory
   if (strlen(currCwd) < strlen(homeCwd) || strcmp((strncat(beginning, currCwd, strlen(homeCwd))), homeCwd) != 0){
     //If current is shorter than home or current doesn't start with home print full path
-    printf("first: %d\n", (strlen(currCwd) < strlen(homeCwd)));
-    printf("second: %d", (strcmp((strncat(beginning, currCwd, strlen(homeCwd))), homeCwd) != 0));
     printf("%s $ ", currCwd);
     fflush(stdout);
   }
